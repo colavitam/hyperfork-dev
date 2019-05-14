7bd6172e39a3b04691f11f997775beaf6a186423
David Gardner <davjgardner@gmail.com>
2019-05-14 15:56:03 -0400

    Simple dd RAM benchmark

---
+++ ddbench.sh
@@ -0,0 +1,17 @@
+#!/bin/busybox sh
+
+# Simple dd RAM benchmark:
+# write some memory (using tmpfs) and then write over it again
+
+FILE=/tmp/ddbench.bin
+BS=1M
+COUNT=1000
+
+function signal_host {
+    echo $1
+}
+
+dd if=/dev/urandom of=$FILE bs=$BS count=COUNT
+signal_host "allocated file"
+dd if=/dev/urandom of=$FILE bs=$BS count=COUNT
+signal_host "done"
