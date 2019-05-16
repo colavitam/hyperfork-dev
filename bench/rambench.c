9c2839bf451204816653b28e755749619cd029d8
David Gardner <davjgardner@gmail.com>
2019-05-16 10:05:29 -0400

    rambench.c: take amount of memory to allocate as cmdline arg

--- bench/rambench.c
+++ bench/rambench.c
@@ -5,31 +5,42 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
 #include <sys/mman.h>
-#include <assert.h>
 #include "comm.h"
 
-#define NPAGES 1000000
 #define PGSIZE 4096
 #define WRITE_SIZE 128
 
 int main(int argc, char **argv)
 {
-	char *mem = mmap(NULL, (size_t) NPAGES * PGSIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
-	assert(mem > 0);
+	if (argc < 2) {
+		printf("Usage: rambench size\nWhere `size` is the amount of memory to allocate in bytes.\n");
+		exit(1);
+	}
 
-	for (unsigned i = 0; i < NPAGES; i++) {
+	unsigned long size = atol(argv[1]);
+
+	char *mem = mmap(NULL, size, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, -1, 0);
+	if (mem == (char *) -1) {
+		printf("Error allocating memory\n");
+		exit(1);
+	}
+
+	for (unsigned i = 0; i < size / PGSIZE; i++) {
 		int *addr = (int *) (mem + i * PGSIZE);
-		for (unsigned j = 0; j < WRITE_SIZE / sizeof(int); j++) {
+		for (unsigned j = 0; j < WRITE_SIZE / sizeof(int) &&
+			     addr + j < (int *) (mem + size); j++) {
 			addr[j] = random();
 		}
 	}
 
 	comm_fork();
 
-	for (unsigned i = 0; i < NPAGES; i++) {
+	for (unsigned i = 0; i < size / PGSIZE; i++) {
 		int *addr = (int *) (mem + i * PGSIZE);
-		for (unsigned j = 0; j < WRITE_SIZE / sizeof(int); j++) {
+		for (unsigned j = 0; j < WRITE_SIZE / sizeof(int) &&
+			     addr + j < (int *) (mem + size); j++) {
 			addr[j] = random();
 		}
 	}
